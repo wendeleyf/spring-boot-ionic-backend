bac516a971fbc1123abae01deb55e1a42fbac9ac
Wendeley Fernandes <wendeleyti@gmail.com>
2019-05-21 14:12:22 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/wendeleyf/cursomc/services/CategoriaService.java
+++ src/main/java/com/wendeleyf/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.wendeleyf.cursomc.domain.Categoria;
 import com.wendeleyf.cursomc.repositories.CategoriaRepository;
+import com.wendeleyf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+ id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
