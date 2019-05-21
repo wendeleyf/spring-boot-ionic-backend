ff4b168afabc4ba87d7863fc4ff413f29e3f9d9f
Wendeley Fernandes <wendeleyti@gmail.com>
2019-05-21 17:43:45 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/wendeleyf/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.wendeleyf.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wendeleyf.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
